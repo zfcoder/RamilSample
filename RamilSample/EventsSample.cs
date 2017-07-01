79e765c9d75a11e15979b971271ea0da820011a8
zfcoder <fayilzig@gmail.com>
2017-07-01 22:12:25 +0300

    Пример проекта с событиями

---
+++ RamilSample/EventsSample.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace RamilSample
+{
+    public delegate void DelSample();
+
+    public class EventsSample
+    {
+        public event DelSample eventSample;
+
+        public EventsSample()
+        {
+            eventSample = Run;
+            eventSample += Run2;
+        }
+
+        public void Start()
+        {
+            if (eventSample != null) 
+                eventSample();
+        }
+
+        private void Run()
+        {
+            Console.Write("Run!");
+        }
+
+        private void Run2()
+        {
+            Console.Write("Run2!");
+        }
+    }
+}
